1c6e16122a64b0e3fb70a753e7a32bd4e5b26fca
Rarysson Guilherme <r_guilherme12@hotmail.com>
2019-02-06 17:19:58 -0300

    Concertado calculo da dispersão da câmera

--- ray/material.cpp
+++ ray/material.cpp
@@ -6,7 +6,7 @@
 
 Raio Liso::dispersar(const Raio& raio_origem, const ObjIntersecao& obj_interceptado) {
 	Vec3 l = (raio_origem.origem - obj_interceptado.ponto_interceptado).normalizar();
-	Vec3 n = obj_interceptado.esfera.normal(obj_interceptado.ponto_interceptado);
+	Vec3 n = obj_interceptado.objeto.normal(obj_interceptado.ponto_interceptado);
 	Vec3 vet_re = (2 * l.produto_escalar(n) * n) - l;
 
 	return Raio(obj_interceptado.ponto_interceptado, vet_re);
